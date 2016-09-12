a32ff6fd2d568b589922f1a8925d3c53f74a59ec
Maurizio Attanasi <maurizio.attanasi@hotmail.com>
2016-09-12 13:49:53 +0200

    Added temperature/humidity reading

--- mqtt/Javascript/remote/index.js
+++ mqtt/Javascript/remote/index.js
@@ -3,13 +3,22 @@ var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://broker.hivemq.com');
 
 var connected = false;
+var setPoint = 0.0;
+var currTemp = null;
+var currHumidity = null;
 
 // unique identifier of the device
 var uid = '33f58d9d-9b78-4a95-b858-7524d089b520';
 var onlineTopic = uid + '/thermostat/online';
+var temperatureTopic = uid + 'thermostat/temp';
+var humidityTopic = uid + 'thermostat/humidity';
+var temperatureSetpointTopic = uid + 'thermostat/temperatureSetpoint';
 
 client.on('connect', () => {
 	client.subscribe(onlineTopic);
+	client.subscribe(temperatureTopic);
+	client.subscribe(humidityTopic);
+	client.subscribe(temperatureSetpointTopic);
 })
 
 client.on('message', (topic, message) => {
@@ -17,6 +26,21 @@ client.on('message', (topic, message) => {
 		case onlineTopic: {
 			connected = (message.toString() === 'true');	
 			console.log('Thermostat is %s', (connected) ? 'online' : 'offline' );		
+		} break;
+
+		case temperatureTopic: {
+				currTemp = JSON.parse(message);
+				console.log('Temperature: %s° @ ', currTemp.value.toFixed(2) , currTemp.timestamp);
+		} break;
+
+		case humidityTopic: {
+				currHumidity = JSON.parse(message);
+				console.log('Humidity: %s% @ ', currHumidity.value.toFixed(2) , currHumidity.timestamp);
+		} break;
+
+		case temperatureSetpointTopic: {
+			setPoint = JSON.parse(message);
+			console.log('Set point %s', setPoint.toFixed(2));
 		} break;
 	}
 })
--- mqtt/Javascript/thermostat/index.js
+++ mqtt/Javascript/thermostat/index.js
@@ -1,14 +1,26 @@
+//  Thermostat
+
 var mqtt = require('mqtt');
+var cron = require('cron');
 
 // unique identifier of the device
 var uid = '33f58d9d-9b78-4a95-b858-7524d089b520';
 
 var onlineTopic = uid + '/thermostat/online';
+var temperatureTopic = uid + 'thermostat/temp';
+var humidityTopic = uid + 'thermostat/humidity';
+var temperatureSetpointTopic = uid + 'thermostat/temperatureSetpoint';
+
+var setPoint = 25;
+var currTemp = getRandomValue(setPoint - 1, setPoint + 1);
+var currHumidity = getRandomValue(40, 60);
 
 var client = mqtt.connect('mqtt://broker.hivemq.com', {
     will:{
         topic: onlineTopic,
-        payload: 'false'
+        payload: 'false',
+        qos: 1,
+        retain: true
     }
 });
 
@@ -17,5 +29,37 @@ client.on('connect', () => {
         qos: 1,
         retain: true
     });
+
     console.log('%s true', onlineTopic);
+
+	client.publish(temperatureSetpointTopic, JSON.stringify( setPoint ), {
+        qos: 1,
+        retain: true
+    });
+
+	console.log('Set point %s', setPoint.toFixed(2));
 })
+
+// Periodic task (triggers every minute)
+var job = new cron.CronJob('* * * * *', function() {  
+
+	currTemp = getRandomValue(setPoint - 1, setPoint + 1);
+    client.publish(temperatureTopic, JSON.stringify(currTemp), {
+    	qos: 1
+    });
+
+	console.log('Temperature: %s° @ ', currTemp.value.toFixed(2) , currTemp.timestamp);
+
+	var currHumidity = getRandomValue(40, 60);
+    client.publish(humidityTopic, JSON.stringify(currHumidity), {
+    	qos: 1
+    });
+
+    console.log('Humidity: %s% @ ', currHumidity.value.toFixed(2) , currHumidity.timestamp);
+
+}, null, true);
+
+// Noise generator
+function getRandomValue(min, max) {	
+	return { value: (Math.random()*(max - min)) + min, timestamp: Date.now() };
+}
