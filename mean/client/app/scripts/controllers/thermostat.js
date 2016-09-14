bb8953e10d7270ba5f186a5b6d8b6ecb7eec828f
Maurizio Attanasi <maurizio.attanasi@hotmail.com>
2016-09-14 18:34:57 +0200

    Started chart implementation

--- mean/client/app/scripts/controllers/thermostat.js
+++ mean/client/app/scripts/controllers/thermostat.js
@@ -7,31 +7,40 @@
  * # ThermostatCtrl
  * Controller of the clientApp
  */
-angular.module('clientApp', ['chart.js'])
+angular.module('clientApp')
   .controller('ThermostatCtrl', function ($scope, Thermostat) {
-    $scope.thermostats = Thermostat.getList().$object;
+    var data = Thermostat.getList().$object;
+    $scope.thermostats = data;
 
-    var data = {
-      labels : ["January","February","March","April","May","June","July"],
-      datasets : [
-        {
-          fillColor : "rgba(220,220,220,0.5)",
-          strokeColor : "rgba(220,220,220,1)",
-          pointColor : "rgba(220,220,220,1)",
-          pointStrokeColor : "#fff",
-          data : [65,59,90,81,56,55,40]
-        },
-        {
-          fillColor : "rgba(151,187,205,0.5)",
-          strokeColor : "rgba(151,187,205,1)",
-          pointColor : "rgba(151,187,205,1)",
-          pointStrokeColor : "#fff",
-          data : [28,48,40,19,96,27,100]
-        }
-      ]
-    }
+    $scope.renderer = 'line';
 
-    $scope.myChart = data;
-
- 
+    $scope.temperatureReading =  
+    
+    [
+      {
+        x: 0,
+        y: 0
+      },
+      {
+        x: 1,
+        y: 1
+      },
+      {
+        x: 2,
+        y: 2
+      },
+      {
+        x: 3,
+        y: 3
+      },
+      {
+        x: 4,
+        y: 4
+      },
+      {
+        x: 5,
+        y: 5
+      }
+      
+    ];
   });
